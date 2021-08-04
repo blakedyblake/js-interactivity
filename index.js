17aebe0874b45fb99def1cd74e2d92155f9b0bad
Blake Nuttall <blakedyblake@gmail.com>
2021-08-04 15:53:30 -0600

    completed extra credit

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ function addMovie(event){
 }
 function deleteMovie(event){
     event.target.parentNode.remove();
-    message.textContent = "Movie deleted!"
+    message.textContent = `${event.target.parentNode.textContent} deleted!`
+    revealMessage();
 }
 
 document.querySelector("form").addEventListener("submit",addMovie);
@@ -34,8 +35,17 @@ document.querySelector("form").addEventListener("submit",addMovie);
 //Step 4 Cross off
 function crossOffMovie(event){
     event.target.classList.toggle("checked")
-    if(event.target.classList.contains("checked")) message.textContent = "Movie watched!";
-    else message.textContent = "Movie added back!"
+    if(event.target.classList.contains("checked")){
+        message.textContent = `${event.target.textContent} watched!`;
+    } 
+    else message.textContent = `${event.target.textContent} added back!`
+
+    revealMessage();
 }
 
 
+function revealMessage(){
+    message.classList.remove("hide")
+    setTimeout(function () {message.classList.add("hide")},1000);
+    message.hide(); 
+}
